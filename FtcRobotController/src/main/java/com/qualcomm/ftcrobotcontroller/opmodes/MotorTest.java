9436e9963c7fc8f6f3753bd9c8b195c57101d269
Kevin Sanita <kwsanita@gmail.com>
2015-09-18 13:50:20 -0400

    Added telemetry data to MotorTest
    Add runtime and motor powers telemetry data to MotorTest OpMode,
    uncommented code for third motor

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MotorTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MotorTest.java
@@ -3,7 +3,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 //  Import modules used in file
 
 public class MotorTest extends OpMode
@@ -11,8 +12,11 @@ public class MotorTest extends OpMode
 {
     DcMotor motorLeft;
     DcMotor motorRight;
-    //DcMotor motorClimb;
+    DcMotor motorClimb;
     //  Declare motor names
+
+    private ElapsedTime runtime = new ElapsedTime();
+    //  Create private runtime function using ElapsedTime function
 
     public MotorTest()
     //  Constructer to save information
@@ -27,33 +31,42 @@ public class MotorTest extends OpMode
         motorLeft = hardwareMap.dcMotor.get("left");
         motorRight = hardwareMap.dcMotor.get("right");
         motorRight.setDirection(DcMotor.Direction.REVERSE);
-        //motorClimb = hardwareMap.dcMotor.get("climb");
+        motorClimb = hardwareMap.dcMotor.get("climb");
         //  Set motors to names set in controller app
+
+        runtime.reset();
+        //  Reset run time counter
     }
 
     @Override
     public void loop()
     //  Main loop
     {
-        double yValRight = gamepad1.right_stick_y;
-        double yValLeft = gamepad1.left_stick_y;
+        double yValRight = -gamepad1.right_stick_y;
+        double yValLeft = -gamepad1.left_stick_y;
         //  Save gamepad joystick values
 
         motorRight.setPower(yValRight);
         motorLeft.setPower(yValLeft);
         //  Set motors to joystick values
 
-//        if(gamepad1.right_trigger>0.2)
-//        //  If the right trigger is at least 20% depressed
-//        {
-//            motorClimb.setPower(gamepad1.right_trigger);
-//            //  Set power to right trigger value
-//        }
-//        else if(gamepad1.left_trigger>0.2)
-//        //  If the left trigger is at least 20% depressed
-//        {
-//            motorClimb.setPower(gamepad1.left_trigger);
-//            //  Set power to left trigger value
-//        }
+        if(gamepad1.right_trigger>0.2)
+        //  If the right trigger is at least 20% depressed
+        {
+            motorClimb.setPower(gamepad1.right_trigger);
+            //  Set power to right trigger value
+        }
+        else if(gamepad1.left_trigger>0.2)
+        //  If the left trigger is at least 20% depressed
+        {
+            motorClimb.setPower(gamepad1.left_trigger);
+            //  Set power to left trigger value
+        }
+
+        telemetry.addData("Run Time","running for " + runtime.toString());
+        //  Add runtime to data at bottom of driver station
+        telemetry.addData("Drive Motor Powers","Left = " + motorLeft.getPower() + ", Right = " + motorRight.getPower());
+        //  Add motor powers to data list
+
     }
 }
